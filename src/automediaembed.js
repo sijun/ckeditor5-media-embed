870ef4d7acde2ad012f07d186690ef45377c669f
sijun <0@sijun.kr>
2020-01-31 16:07:52 +0900

    embed on enter

--- src/automediaembed.js
+++ src/automediaembed.js
@@ -9,11 +9,9 @@
 
 import MediaEmbedEditing from './mediaembedediting';
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import { keyCodes } from '@ckeditor/ckeditor5-utils/src/keyboard';
 import LiveRange from '@ckeditor/ckeditor5-engine/src/model/liverange';
 import LivePosition from '@ckeditor/ckeditor5-engine/src/model/liveposition';
-import Undo from '@ckeditor/ckeditor5-undo/src/undo';
-import global from '@ckeditor/ckeditor5-utils/src/dom/global';
 import { insertMedia } from './utils';
 
 const URL_REGEXP = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=]+$/;
@@ -29,7 +27,7 @@ export default class AutoMediaEmbed extends Plugin {
 	 * @inheritDoc
 	 */
 	static get requires() {
-		return [ Clipboard, Undo ];
+		return [];
 	}
 
 	/**
@@ -42,79 +40,34 @@ export default class AutoMediaEmbed extends Plugin {
 	/**
 	 * @inheritDoc
 	 */
-	constructor( editor ) {
-		super( editor );
-
-		/**
-		 * The paste–to–embed `setTimeout` ID. Stored as a property to allow
-		 * cleaning of the timeout.
-		 *
-		 * @private
-		 * @member {Number} #_timeoutId
-		 */
-		this._timeoutId = null;
-
-		/**
-		 * The position where the `<media>` element will be inserted after the timeout,
-		 * determined each time the new content is pasted into the document.
-		 *
-		 * @private
-		 * @member {module:engine/model/liveposition~LivePosition} #_positionToInsert
-		 */
-		this._positionToInsert = null;
-	}
-
-	/**
-	 * @inheritDoc
-	 */
 	init() {
-		const editor = this.editor;
-		const modelDocument = editor.model.document;
+		const view = this.editor.editing.view;
+		const viewDocument = view.document;
 
-		// We need to listen on `Clipboard#inputTransformation` because we need to save positions of selection.
-		// After pasting, the content between those positions will be checked for a URL that could be transformed
-		// into media.
-		this.listenTo( editor.plugins.get( Clipboard ), 'inputTransformation', () => {
-			const firstRange = modelDocument.selection.getFirstRange();
-
-			const leftLivePosition = LivePosition.fromPosition( firstRange.start );
-			leftLivePosition.stickiness = 'toPrevious';
-
-			const rightLivePosition = LivePosition.fromPosition( firstRange.end );
-			rightLivePosition.stickiness = 'toNext';
-
-			modelDocument.once( 'change:data', () => {
-				this._embedMediaBetweenPositions( leftLivePosition, rightLivePosition );
-
-				leftLivePosition.detach();
-				rightLivePosition.detach();
-			}, { priority: 'high' } );
-		} );
-
-		editor.commands.get( 'undo' ).on( 'execute', () => {
-			if ( this._timeoutId ) {
-				global.window.clearTimeout( this._timeoutId );
-				this._positionToInsert.detach();
-
-				this._timeoutId = null;
-				this._positionToInsert = null;
+		this.listenTo( viewDocument, 'keydown', ( eventInfo, domEventData ) => {
+			const keyCode = domEventData.keyCode;
+			if ( keyCode == keyCodes.enter ) {
+				if ( this._embedMedia() ) {
+					domEventData.preventDefault();
+					eventInfo.stop();
+				}
 			}
 		}, { priority: 'high' } );
 	}
 
-	/**
-	 * Analyzes the part of the document between provided positions in search for a URL representing media.
-	 * When the URL is found, it is automatically converted into media.
-	 *
-	 * @protected
-	 * @param {module:engine/model/liveposition~LivePosition} leftPosition Left position of the selection.
-	 * @param {module:engine/model/liveposition~LivePosition} rightPosition Right position of the selection.
-	 */
-	_embedMediaBetweenPositions( leftPosition, rightPosition ) {
+	_embedMedia() {
+		const rightPosition = this.editor.model.document.selection.focus;
+		const leftPosition = this.editor.model.document.selection.focus;
+		const leftLivePosition = LivePosition.fromPosition( leftPosition );
+		leftLivePosition.offset = 0;
+		leftLivePosition.stickiness = 'toPrevious';
+		const rightLivePosition = LivePosition.fromPosition( rightPosition );
+		rightLivePosition.stickiness = 'toNext';
+
 		const editor = this.editor;
 		const mediaRegistry = editor.plugins.get( MediaEmbedEditing ).registry;
-		// TODO: Use marker instead of LiveRange & LivePositions.
-		const urlRange = new LiveRange( leftPosition, rightPosition );
+
+		const urlRange = new LiveRange( leftLivePosition, rightLivePosition );
 		const walker = urlRange.getWalker( { ignoreElementEnd: true } );
 
 		let url = '';
@@ -127,46 +80,37 @@ export default class AutoMediaEmbed extends Plugin {
 
 		url = url.trim();
 
+		let embedded = true;
+
 		// If the URL does not match to universal URL regexp, let's skip that.
 		if ( !url.match( URL_REGEXP ) ) {
-			return;
+			embedded = false;
 		}
 
 		// If the URL represents a media, let's use it.
 		if ( !mediaRegistry.hasMedia( url ) ) {
-			return;
+			embedded = false;
 		}
 
 		const mediaEmbedCommand = editor.commands.get( 'mediaEmbed' );
 
 		// Do not anything if media element cannot be inserted at the current position (#47).
 		if ( !mediaEmbedCommand.isEnabled ) {
-			return;
+			embedded = false;
 		}
 
-		// Position won't be available in the `setTimeout` function so let's clone it.
-		this._positionToInsert = LivePosition.fromPosition( leftPosition );
-
-		// This action mustn't be executed if undo was called between pasting and auto-embedding.
-		this._timeoutId = global.window.setTimeout( () => {
+		if ( embedded ) {
 			editor.model.change( writer => {
-				this._timeoutId = null;
-
+				insertMedia( editor.model, url, leftLivePosition );
+				writer.setSelection( urlRange );
 				writer.remove( urlRange );
-
-				let insertionPosition;
-
-				// Check if position where the media element should be inserted is still valid.
-				// Otherwise leave it as undefined to use document.selection - default behavior of model.insertContent().
-				if ( this._positionToInsert.root.rootName !== '$graveyard' ) {
-					insertionPosition = this._positionToInsert;
-				}
-
-				insertMedia( editor.model, url, insertionPosition );
-
-				this._positionToInsert.detach();
-				this._positionToInsert = null;
+				writer.setSelection( rightLivePosition );
 			} );
-		}, 100 );
+		}
+
+		leftLivePosition.detach();
+		rightLivePosition.detach();
+
+		return embedded;
 	}
 }
